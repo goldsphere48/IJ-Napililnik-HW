a9b2178c155ceca4283fc8b5e9fb43f29d0db6d5
Viktor Titov <vtitov@game-forest.com>
2021-11-29 18:47:06 +1000

    Complete Task 7

--- Napilnik/Napilnik/Program.cs
+++ Napilnik/Napilnik/Program.cs
@@ -1,5 +1,19 @@
 ﻿static class Program
 {
+	class Weapon
+	{
+		private int _bullets;
+
+		private bool CanShoot() => _bullets > 0;
+
+		public void Shoot()
+		{
+			if (CanShoot()) {
+				_bullets -= 1;
+			}
+		}
+	}
+
 	public static void Main()
 	{
 
