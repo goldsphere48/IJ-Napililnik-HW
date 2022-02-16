82a60f6cfdc6cff07fa326756ff167f165e6a31e
Viktor Titov <vtitov@game-forest.com>
2022-02-16 18:29:51 +1000

    Complete task

--- Napilnik/Napilnik/Program.cs
+++ Napilnik/Napilnik/Program.cs
@@ -1,7 +1,35 @@
-﻿static class Program
+﻿class Weapon
 {
-	public static void Main()
-	{
+    public float Cooldown { get; private set; }
+    public int Damage { get; private set; }
+}
 
-	}
+class Movement
+{
+    public float DirectionX { get; private set; }
+    public float DirectionY { get; private set; }
+    public float Speed { get; private set; }
+}
+
+class Player
+{
+    public string Name { get; private set; }
+    public int Age { get; private set; }
+    public Weapon Weapon { get; private set; }
+    public Movement Movement { get; private set; }
+
+    public void Move()
+    {
+        //Do move
+    }
+
+    public void Attack()
+    {
+        //attack
+    }
+
+    public bool IsReloading()
+    {
+        throw new NotImplementedException();
+    }
 }
