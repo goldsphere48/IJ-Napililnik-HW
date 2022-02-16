c3f5ae21c9ab55e84d8b9f2884b092629239be1b
Viktor Titov <vtitov@game-forest.com>
2022-02-16 20:03:43 +1000

    Complete task

--- Napilnik/Napilnik/Program.cs
+++ Napilnik/Napilnik/Program.cs
@@ -1,6 +1,20 @@
-﻿static class Program
+﻿class Program
 {
-	public static void Main()
+    private bool _enable;
+
+    public void Enable()
+    {
+        _enable = true;
+        _effects.StartEnableAnimation();
+    }
+
+    public void Disable()
+    {
+        _enable = false;
+        _pool.Free(this);
+    }
+
+    public static void Main()
 	{
 
 	}
